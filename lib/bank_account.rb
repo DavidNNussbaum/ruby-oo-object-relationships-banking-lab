3013abdf309021fd91ffeea3ae74ced8a6d98b5e
DavidNNussbaum <brocknynj@aol.com>
2020-11-16 19:52:30 -0500

    commit tree f57d00c85935b35a10ae676d066cec04c930ec07

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,56 @@
-class BankAccount
+# require 'pry'
+# class BankAccount
+#     attr_accessor :balance, :status
+#     attr_reader :name
 
-end
+# def initialize(name)
+#     @name = name
+#     @balance = 1000
+#     @status = "open"
+# end
+
+# def deposit(amount)
+#     self.balance += amount
+# end
+
+# def display_balance
+#     return "Your balance is $#{self.balance}."
+# end
+
+# def valid?
+#     status == "open" && balance > 0
+# end
+
+# def close_account
+#     @status = "closed"
+# end
+
+# end
+class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+  
+    def initialize(name)
+      @name = name
+      @balance = 1000
+      @status = "open"
+    end
+  
+    def deposit(deposit_amount)
+      self.balance += deposit_amount
+    end
+  
+    def display_balance
+      "Your balance is $#{balance}."
+    end
+  
+    def close_account
+      self.status = "closed"
+    end
+  
+    def valid?
+      balance > 0 && status == "open"
+    end
+  
+  end
+  
